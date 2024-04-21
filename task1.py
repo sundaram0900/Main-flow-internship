39e6bf723a5752135d0adb718b23f15729ffcdb7
Sundaram kocherlakota <133401816+sundaram0900@users.noreply.github.com>
2024-04-21 20:06:56 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,33 @@
+my_list = [1, 2, 3]
+print("Original list:", my_list)
+
+my_list.append(4)
+print("List after adding an element:", my_list)
+
+my_list.remove(2)
+print("List after removing an element:", my_list)
+
+my_list[1] = 5
+print("List after modifying an element:", my_list)
+
+my_dict = {'a': 1, 'b': 2, 'c': 3}
+print("\nOriginal dictionary:", my_dict)
+
+my_dict['d'] = 4
+print("Dictionary after adding a key-value pair:", my_dict)
+
+del my_dict['b']
+print("Dictionary after removing a key-value pair:", my_dict)
+
+my_dict['a'] = 5
+print("Dictionary after modifying a value:", my_dict)
+
+my_set = {1, 2, 3}
+print("\nOriginal set:", my_set)
+
+my_set.add(4)
+print("Set after adding an element:", my_set)
+
+my_set.remove(2)
+print("Set after removing an element:", my_set)
+
